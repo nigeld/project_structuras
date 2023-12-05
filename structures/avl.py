92f1b6a7d6f175a195892595b3afd6d5dfcc1829
Camila Castro <mcastropo@unal.edu.co>
2023-12-05 00:08:24 -0500

    add avl tree

---
+++ structures/avl.py
@@ -0,0 +1,135 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def getHeight(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def getBalance(self, node):
+        if not node:
+            return 0
+        return self.getHeight(node.left) - self.getHeight(node.right)
+
+    def rotateLeft(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        return y
+
+    def rotateRight(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+
+        return x
+
+    def insert(self, root, key):
+        if not root:
+            return Node(key)
+        elif key < root.key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        if balance > 1 and key < root.left.key:
+            return self.rotateRight(root)
+
+        if balance < -1 and key > root.right.key:
+            return self.rotateLeft(root)
+
+        if balance > 1 and key > root.left.key:
+            root.left = self.rotateLeft(root.left)
+            return self.rotateRight(root)
+
+        if balance < -1 and key < root.right.key:
+            root.right = self.rotateRight(root.right)
+            return self.rotateLeft(root)
+
+        return root
+
+    def minValueNode(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def delete(self, root, key):
+        if not root:
+            return root
+
+        if key < root.key:
+            root.left = self.delete(root.left, key)
+        elif key > root.key:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.minValueNode(root.right)
+            root.key = temp.key
+            root.right = self.delete(root.right, temp.key)
+
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        if balance > 1 and self.getBalance(root.left) >= 0:
+            return self.rotateRight(root)
+
+        if balance < -1 and self.getBalance(root.right) <= 0:
+            return self.rotateLeft(root)
+
+        if balance > 1 and self.getBalance(root.left) < 0:
+            root.left = self.rotateLeft(root.left)
+            return self.rotateRight(root)
+
+        if balance < -1 and self.getBalance(root.right) > 0:
+            root.right = self.rotateRight(root.right)
+            return self.rotateLeft(root)
+
+        return root
+
+    def search(self, root, key):
+        if root is None or root.key == key:
+            return root
+
+        if root.key < key:
+            return self.search(root.right, key)
+
+        return self.search(root.left, key)
+
+    def preOrder(self, root):
+        if root:
+            print("{0} ".format(root.key))
+            self.preOrder(root.left)
+            self.preOrder(root.right)
